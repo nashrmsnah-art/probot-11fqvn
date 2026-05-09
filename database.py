7b28a631a4ddad2e71591610412173fa57e5dd60
nashrmsnah-art <nashrmsnah@gmail.com>
2026-05-09 09:16:35 +0300

    Update database.py

--- database.py
+++ database.py
@@ -2,20 +2,22 @@ import json
 
 class BotDB:
     def __init__(self):
-        self.path = "bot_data.json"
+        self.file = "system_data.json"
         try:
-            with open(self.path, "r") as f: self.data = json.load(f)
+            with open(self.file, "r") as f: self.data = json.load(f)
         except:
-            self.data = {"locked": False, "users": {}, "total_reports": 0}
+            self.data = {"locked": False, "users": {}, "total_ops": 0}
 
     def save(self):
-        with open(self.path, "w") as f: json.dump(self.data, f)
+        with open(self.file, "w") as f: json.dump(self.data, f)
 
-    def get_lang(self, uid):
-        return self.data["users"].get(str(uid), {}).get("lang", "en")
+    def get_user(self, uid):
+        uid = str(uid)
+        if uid not in self.data["users"]:
+            self.data["users"][uid] = {"lang": "en", "is_auth": False}
+            self.save()
+        return self.data["users"][uid]
 
     def set_lang(self, uid, lang):
-        if str(uid) not in self.data["users"]: 
-            self.data["users"][str(uid)] = {"reports": 0}
-        self.data["users"][str(uid)]["lang"] = lang
+        self.get_user(uid)["lang"] = lang
         self.save()
